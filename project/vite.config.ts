94fee622fc98d3cca7a154786aa8474fd6e656be
vishal bhoi <vbhoi4513@gmail.com>
2025-12-16 17:22:52 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,8 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: "/shri/",
+  plugins: [react()],
+})
+
     return {
       server: {
         port: 3000,
